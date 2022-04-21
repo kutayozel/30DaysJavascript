94b9a418b2fb1eb44468db25c0e98783542f47c4
Kharza <zelkutay@gmail.com>
2022-04-21 20:54:08 +0300

    reduce

---
+++ AddingUpwReduce/index.js
@@ -0,0 +1,19 @@
+const timeNodes = document.querySelectorAll('[data-time]')
+
+const timeArray = [...timeNodes]
+
+const seconds = timeArray.map(node => node.dataset.time)
+.map(timeCode => {
+    const [mins, secs] = timeCode.split(':').map(parseFloat)
+    return (mins * 60) + secs
+    console.log(mins, secs)
+})
+.reduce
+((total, vidSeconds) => total + vidSeconds)
+
+let secondsLeft = seconds
+const hours = Math.floor(secondsLeft / 3600)
+secondsLeft = secondsLeft % 3600
+//console.log(secondsLeft)
+const mins = Math.floor(secondsLeft / 60)
+secondsLeft = secondsLeft % 60
